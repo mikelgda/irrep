3bee178c8a541779ae6362566a43dc4a7504927d
Mikel García <mikelgda@users.noreply.github.com>
2021-02-08 13:04:32 +0100

    Fixes to characters

--- irreptables/__init__.py
+++ irreptables/__init__.py
@@ -470,5 +470,3 @@ class IrrepTable:
                 except Exception as err:
                     logger.debug("error while reading irrep <{0}>".format(l), err)
                     pass
-        print("TABLE LOADED")
-        self.show()
