82a6ad5650124b284ba258754563ef81a83b58ed
Mikel García <mikelgda@users.noreply.github.com>
2021-03-11 19:35:46 +0100

    Trace functionality and comments

--- irrep/kdotpmodel.py
+++ irrep/kdotpmodel.py
@@ -4,13 +4,37 @@ import matplotlib.pyplot as plt
 
 
 class KdotPModel:
-    def __init__(self, symgroup, kpoint, kpname, rep, h, p, user_defined = None):
+    """
+    Given a space group and a maximal kpoint of the BZ, creates a k.p model representation
+    whose eigenstates belong to a given representation and for the Hamiltonian supplied.
+    Includes common operations with this kind of model:
+        - Plotting the energies along one k-line
+        - Computing the eigenvalues and eigenstates at a given kpoint
+        - Computing the traces of the representation
+        - Finding the irrep decomposition of the eigenstates after a symmetry-breaking
+          perturbation is introduced, given the target space group and kpoint. (WIP)
+    """
+    def __init__(self, symgroup, kpoint, kpname, rep, h, p, user_defined = None, hermitian = True):
+        """
+        Define the space symmetry group and the specific kpoint of the expansion.
+        The small irreps may be loaded from the tables or user defined. In that case
+        'user_defined' should contain the path to the text file containing the operation
+        names and their matrix representation (see '_load_custom').
+        Otherwise, rep should be a list (even if only one present) of the standard irrep names. When
+        more than one name is given, this indicates the direct sum of the representations.
+        'hamiltonian' is a matrix function of the same dimension as 'rep' which is expected
+        to have two arguments: a 3-tuple of the kpoint and a tuple 'p' of the parameters of the model.
+        'p' is supplied at the moment of creation of the KdotPModel instance.
+
+        The model is implicitly assumed to be Hermitian. If not, set 'hermitian=False' by keyword argument.
+        """
         self.symgroup = symgroup
         self.kpoint = kpoint
         self.kpname = kpname
         self.rep = rep
         self.hamiltonian = h
         self.parameters = p
+        self.hermitian = hermitian
         
         if user_defined:
             self.opnames, self.matrices = self._load_custom(user_defined)
@@ -18,6 +42,11 @@ class KdotPModel:
             self.opnames, self.matrices = self._load_irreps()
     
     def _load_irreps(self):
+        """
+        Loads small irrep matrices from the standard tables of the BCS.
+        Returns an array of Seitz symbols and their corresponding matrices, in the same
+        order.
+        """
         
         def build_matrix(string):
             string = string.split()
@@ -41,6 +70,16 @@ class KdotPModel:
         return table['symbol'].values, irrepmats
     
     def _load_custom(self,path):
+        """
+        Loads a user defined representation. The file should be formatted as follows
+            1. Names for the operations, space-separated
+            2. Operations in matrix form, e.g.
+
+                    0 1j 0
+                    -1j 0 0
+                    0 0 1
+                Matrices should be separated by a line break.
+        """
         def tokenize(lines):
             chunk = []
             nl = len(lines)
@@ -64,12 +103,47 @@ class KdotPModel:
         
         return opnames, np.array(mats, dtype = complex)
     
-    def plot_line(self, line = 'z', kstart = (0,0,-1), kend = (0,0,1), npoints = 100):
+    def plot_line(self, kstart = (0,0,-1), kend = (0,0,1), npoints = 100):
+        """
+        Plots the bands along a line given by evenly spaced values between the 3-tuples
+        kstart and kend.
+        """
         
         kline = np.linspace(kstart,kend,npoints)
-        energies = np.array([np.sort(np.linalg.eigvals(self.hamiltonian(kpoint,self.parameters))) for kpoint in kline])
+        if self.hermitian:
+            energies = np.array([np.sort(np.linalg.eigvalsh(self.hamiltonian(kpoint,self.parameters))) for kpoint in kline])
+        else:
+            energies = np.array([np.sort(np.linalg.eigvalsh(self.hamiltonian(kpoint,self.parameters))) for kpoint in kline])
         
-        plt.plot(energies,color = 'red')
+        plt.plot(energies, color = 'red')
+    
+    def energies(self, kpoint=(0,0,0)):
+        if self.hermitian:
+            return np.linalg.eigvalsh(self.hamiltonian(kpoint,self.parameters))
+        else:
+            return np.linalg.eigvals(self.hamiltonian(kpoint,self.parameters))
+
+    def eigenstates(self,kpoint=(0,0,0)):
+        if self.hermitian:
+            return np.linalg.eigh(self.hamiltonian(kpoint,self.parameters))
+        else:
+            return np.linalg.eig(self.hamiltonian(kpoint,self.parameters))
+    
+    def traces(self,kpoint=(0,0,0)):
+        """
+        Computes the eigenvectors at a given kpoint and returns the traces of
+        the operations, in the same order as in 'self.matrices' or 'self.opnames'.
+        """
+        states=self.eigenstates(kpoint)[1]
+        traces = []
+        for matrix in self.matrices:
+            traces.append(self._compute_trace(states,matrix))
+        return np.array(traces)
+    
+    def _compute_trace(self,vecs,operation):
+        return np.trace(np.transpose(vecs) @ operation @ vecs)
+            
+
             
         
         
